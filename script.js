3e3f2b0863d10c834b3d74b18a736a5f32a8e1ec
Brett W <brett.w.wiese@gmail.com>
2018-10-12 18:45:49 -0400

    solitaire and start menu link

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-    $('.windows-box').click(function(e) {
-        $('.windows-box').css('z-index', '1');
-        $(this).css('z-index', '5');
-    });
+$('.windows-box').click(function(e) {
+    $('.windows-box').css('z-index', '1');
+    $(this).css('z-index', '5');
+});
 
-    $('.windows-box').each(function() {
-        $(this).draggable();
-    });
+$('.drag-me').each(function() {
+    $(this).draggable();
+});
 
-    $('.apple-button').click(function() {
-	  	$("#apple-window").toggleClass("close");
-	});
-
-
-	$('.spotify-button').click(function() {
-	  	$("#spotify-window").toggleClass("close");
-	});
-
-	$('.ie-button').click(function() {
-	  	$("#ie-window").toggleClass("close");
-	});
+$('.apple-button').click(function() {
+  	$("#apple-window").toggleClass("close");
+});
 
 
-	$('.start-button').click(function() {
-	  	$("#start-menu").toggleClass("close");
+$('.spotify-button').click(function() {
+  	$("#spotify-window").toggleClass("close");
+});
 
-	});
-	
+$('.ie-button').click(function() {
+  	$("#ie-window").toggleClass("close");
+});
+
+$('.solitaire-button').click(function() {
+  	$("#solitaire-window").toggleClass("close");
+});
+
+
+
+
+var modal = document.getElementById('start')
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+    	console.log('in if statement')
+        modal.style.display = "none";
+    }
+}
