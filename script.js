4b3f1247e0cbac2d042b764eb158b5e0db014555
Brett W <brett.w.wiese@gmail.com>
2018-10-17 00:24:16 -0400

    icon fixing, load screen

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//loading screen
+$(document).ready(function () {
+    setTimeout(function(){
+        $('#loading').toggleClass("close");
+    }, 200);
+});
+
+
+// window dragging - general
 $('.windows-box').click(function(e) {
     $('.windows-box').css('z-index', '1');
     $(this).css('z-index', '5');
@@ -7,29 +16,39 @@ $('.drag-me').each(function() {
     $(this).draggable();
 });
 
+//closing windows and putting them on top
 $('.apple-button').click(function() {
   	$("#apple-window").toggleClass("close");
+  	$('.windows-box').css('z-index', '1');
+    $("#apple-window").css('z-index', '5');
 });
-
 
 $('.spotify-button').click(function() {
   	$("#spotify-window").toggleClass("close");
+  	    $('.windows-box').css('z-index', '1');
+    $("#spotify-window").css('z-index', '5');
 });
 
 $('.ie-button').click(function() {
   	$("#ie-window").toggleClass("close");
+  	$('.windows-box').css('z-index', '1');
+    $("#ie-window").css('z-index', '5');
 });
 
 $('.solitaire-button').click(function() {
   	$("#solitaire-window").toggleClass("close");
+  	$('.windows-box').css('z-index', '1');
+    $("#solitaire-window").css('z-index', '5');
 });
 
+//solitaire loader
 $(document).ready(function () {
     setTimeout(function(){
         $('#solitaire-window').toggleClass("close");
-    }, 10);
+    }, 100);
 });
 
+//start menu funcionality
 var modal = document.getElementById('start')
 
 window.onclick = function(event) {
