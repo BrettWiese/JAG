f10c69f0b9212b0d1f79fa53a4d9d7e534fb36a9
Brett W <brett.w.wiese@gmail.com>
2018-10-28 12:09:45 -0400

    email sub and mobile

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     setTimeout(function(){
         $('#loading').toggleClass("close");
-    }, 200);
+    }, 0);
 });
 
 
@@ -18,28 +18,115 @@ $('.drag-me').each(function() {
 
 //closing windows and putting them on top
 $('.apple-button').click(function() {
+    if ( $(window).width() > 600) {
   	$("#apple-window").toggleClass("close");
   	$('.windows-box').css('z-index', '1');
-    $("#apple-window").css('z-index', '5');
+    $("#apple-window").css('z-index', '    5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#apple-window").offset().top-20},500);
+    }
 });
 
 $('.spotify-button').click(function() {
+    if ( $(window).width() > 600) {
   	$("#spotify-window").toggleClass("close");
-  	    $('.windows-box').css('z-index', '1');
-    $("#spotify-window").css('z-index', '5');
+  	$('.windows-box').css('z-index', '1');
+    $("#spotify-window").css('z-index', '    5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#spotify-window").offset().top-20},500);
+    }
 });
 
 $('.ie-button').click(function() {
+    if ( $(window).width() > 600) {
   	$("#ie-window").toggleClass("close");
   	$('.windows-box').css('z-index', '1');
-    $("#ie-window").css('z-index', '5');
+    $("#ie-window").css('z-index', '    5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#ie-window").offset().top-20},500);
+    }
 });
 
 $('.solitaire-button').click(function() {
+    if ( $(window).width() > 600) {
   	$("#solitaire-window").toggleClass("close");
   	$('.windows-box').css('z-index', '1');
     $("#solitaire-window").css('z-index', '5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#solitaire-window").offset().top-20},500);
+    }
 });
+
+$('.mail-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#mail-window").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#mail-window").css('z-index', '5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#mail-window").offset().top-20},500);
+    }
+});
+
+$('.notepad-window-1-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#notepad-window-1").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#notepad-window-1").css('z-index', '5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#notepad-window-1").offset().top-20},500);
+    }
+});
+
+$('.notepad-window-2-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#notepad-window-2").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#notepad-window-2").css('z-index', '5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#notepad-window-2").offset().top-20},500);
+    }
+});
+
+$('.notepad-window-3-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#notepad-window-3").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#notepad-window-3").css('z-index', '5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#notepad-window-3").offset().top-20},500);
+    }
+});
+
+$('.notepad-window-4-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#notepad-window-4").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#notepad-window-4").css('z-index', '    5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#notepad-window-4").offset().top-20},500);
+    }
+});
+
+$('.notepad-window-5-button').click(function() {
+    if ( $(window).width() > 600) {
+    $("#notepad-window-5").toggleClass("close");
+    $('.windows-box').css('z-index', '1');
+    $("#notepad-window-5").css('z-index', '    5');
+    }
+    else {
+      $("body, html").animate({scrollTop: $("#notepad-window-5").offset().top-20},500);
+    }
+});
+
 
 //solitaire loader
 $(document).ready(function () {
